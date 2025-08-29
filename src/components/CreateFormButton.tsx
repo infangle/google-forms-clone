153f4c3f1415a9c6a7a9846fa707e5d2c3db289a
infangle <lakewyetnayet93@gmail.com>
2025-08-29 14:31:26 +0300

    use primeicons

--- src/components/CreateFormButton.tsx
+++ src/components/CreateFormButton.tsx
@@ -1,5 +1,5 @@
-// src/components/forms/CreateFormButton.tsx
 import { Link } from "react-router-dom";
+import 'primeicons/primeicons.css';
 
 export default function CreateFormButton() {
   return (
@@ -14,19 +14,8 @@ export default function CreateFormButton() {
         to="/forms/create"
         className="w-[150px] h-[150px] flex items-center justify-center bg-white border border-gray-200 rounded-sm hover:border-blue-400 transition-colors"
       >
-        {/* Plus icon */}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="50"
-          height="50"
-          viewBox="0 0 24 24"
-          className="text-gray-600"
-        >
-          <path
-            fill="currentColor"
-            d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6z"
-          />
-        </svg>
+        {/* PrimeIcons' pi-plus icon */}
+        <i className="pi pi-plus text-5xl text-gray-600"></i>
       </Link>
     </div>
   );
